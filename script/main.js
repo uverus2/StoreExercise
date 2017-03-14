3b20afcabcc855c09ae9f20e9a33b630a6c41b40
uverus2 <2ruzhk43@solent.ac.uk>
2017-03-14 11:47:17 +0000

    commit

--- script/main.js
+++ script/main.js
@@ -5,7 +5,10 @@
 function main() {
 
     //set up reference to our form, since 
-    var form = document.getElementById("productForm"); 
+    var form = document.getElementById("productForm");
+    form.addEventListener("change",formChanged); /* change listens to change event then formChange activated when form changes */
+    
+   
  
     
 }
@@ -14,5 +17,28 @@ function main() {
   onChange callback function
 **/
 function formChanged(event) {
-    var form = document.getElementById("productForm");     
+    
+    var form = document.getElementById("productForm");   
+    
+    var subTotal = 0;
+    var shipping = 0;
+    var taxes = 0;
+    var total = 0;
+    var choices = "";
+
+for (var=i; i < form.extras.length;i++)
+    
+    if (form.extras[i].checked) {
+        
+        numberProduct = preseInt(form.product[1].dataset.price);
+        subTotal = subTotal + numberTotal;
+        
+        choices=choices + "<p>" + form.extra[i].dataset.data.disc + "</p>"
+        
+        
+        
+    } /* end of if */
+
 }
+
+
