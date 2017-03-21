7e6e51892741a01f3b1a542df539720535836a29
uverus2 <2ruzhk43@solent.ac.uk>
2017-03-21 11:06:13 +0000

    form work

--- script/main.js
+++ script/main.js
@@ -6,12 +6,37 @@ function main() {
 
     //set up reference to our form, since 
     var form = document.getElementById("productForm");
+    
     form.addEventListener("change",formChanged); /* change listens to change event then formChange activated when form changes */
+    form.blender.addEventListener("blur",validateName) /* event blur listens to leaving the form situation then validate name is activated when form is left */
+    
+    
     
    
  
     
 }
+
+
+function validate(name) {
+    
+    var form = document.getElementById("productForm") // sets a reference to the form cuz product for is the id of the form 
+    
+    if (form.name.value =  ){
+        
+        productForm.name.style.backgroundColor="red"
+        return false;   
+    }
+    
+    else {
+        
+        productForm.product.style.backgroundColor="";
+        return true;
+    }
+        
+    
+}
+
 
 /**
   onChange callback function
@@ -24,7 +49,7 @@ function formChanged(event) {
     var shipping = 0;
     var taxes = 0;
     var total = 0;
-    var netTotal = 0;
+    
     var choices = "";
     
      console.log("the form has changed");
@@ -34,8 +59,8 @@ for (var i=0; i < form.product.length;i++) {
     if (form.product[i].checked) {
         
     var numberTotal =  parseInt(form.product[i].dataset.price);
-          netTotal = form.product[i].getElement * numberTotal;
-    subTotal = subTotal +  netTotal; 
+          
+    subTotal = subTotal + numberTotal; 
     
      // choices=choices + "<p>" + form.extra[i].dataset.data.disc + "</p>"
         
@@ -47,7 +72,7 @@ for (var i=0; i < form.product.length;i++) {
     } /* end of if */
     } /* end of loop */
     
-    if (subTotal>100){
+    if (subTotal<100){
         
         shipping=10
     }/* end of if */
